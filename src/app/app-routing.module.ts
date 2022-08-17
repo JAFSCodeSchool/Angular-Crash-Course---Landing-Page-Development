21b49becdd34fb3ec8b1720fa3345d1970768b26
Franklin Anto <franklin@jsam.in>
2022-08-17 14:29:29 +0530

    Adding Routes(Multiple Pages) to our project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCourseComponent } from './all-course/all-course.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'all-course', component: AllCourseComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'pages', component: PagesComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AllCourseComponent } from './all-course/all-course.component';
+import { PagesComponent } from './pages/pages.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllCourseComponent,
+    PagesComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
