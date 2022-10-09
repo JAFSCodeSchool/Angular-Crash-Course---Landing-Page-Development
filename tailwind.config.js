37b6edb3c76f31c19a92f3e84703572f98c88243
Franklin Anto <franklin@jsam.in>
2022-10-10 00:37:18 +0530

    9_complex_hero_section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = AnimateCSS({
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        upDown:'upDown 3s linear infinite',
+        leftRight:'leftRight 3s linear infinite',
+      },
+      keyframes:{
+        upDown:{
+          '0%':{paddingTop:'0px'},
+          '50%':{paddingTop:'40px'},
+          '100%':{paddingTop:'0px'}
+        },
+        leftRight:{
+          '0%':{paddingLeft:'0px'},
+          '50%':{paddingLeft:'40px'},
+          '100%':{paddingLeft:'0px'}
+        }
+      }
+    },
   },
   plugins: [],
 })
